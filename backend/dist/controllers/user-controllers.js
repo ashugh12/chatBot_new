cc01a7cc72db4e2308370a09e3f54c95702c2e81
ym <you@example.com>
2024-06-03 21:24:34 +0530

    Create frontend

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import { compare, hash } from "bcrypt";
 import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 export const getAllUsers = async (req, res, next) => {
     // get all users
     try {
@@ -22,6 +23,17 @@ export const userSignup = async (req, res, next) => {
         const hashedPassword = await hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
+        //create token and store cookie
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/",
+        });
+        const token = createToken(user._id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, { path: "/", domain: "localhost", expires, httpOnly: true, signed: true, });
         return res.status(201).json({ message: "OK", id: user._id.toString() });
     }
     catch (error) {
@@ -41,11 +53,16 @@ export const userLogin = async (req, res, next) => {
         if (!isPasswordCorrect) {
             return res.status(403).send("Incorrect Password");
         }
-        res.clearCookie('auth_token');
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/",
+        });
         const token = createToken(user._id.toString(), user.email, "7d");
         const expires = new Date();
         expires.setDate(expires.getDate() + 7);
-        res.cookie("auth_token", token, { path: "/", domain: "localhost", expires, httpOnly: true, signed: true, });
+        res.cookie(COOKIE_NAME, token, { path: "/", domain: "localhost", expires, httpOnly: true, signed: true, });
         return res.status(200).json({ message: "OK", id: user._id.toString() });
     }
     catch (error) {
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js"
 import { compare, hash } from "bcrypt"
 import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 export const getAllUsers = async(
     req:Request,
     res: Response,
@@ -32,6 +33,21 @@ export const userSignup = async(
         const hashedPassword = await hash(password, 10);
         const user = new User({name, email, password:hashedPassword})
         await user.save();
+
+        //create token and store cookie
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true, 
+            domain: "localhost",
+            signed: true,
+            path: "/",
+        })
+
+
+        const token = createToken(user._id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate()+7);
+        res.cookie(COOKIE_NAME, token, {path:"/", domain:"localhost", expires,httpOnly:true, signed:true,})
+        
         return res.status(201).json({message:"OK", id: user._id.toString()})
     }catch(error){
         console.log(error)
@@ -56,11 +72,20 @@ export const userLogin = async(
         const isPasswordCorrect = await compare(password, user.password)
         if(!isPasswordCorrect){return res.status(403).send("Incorrect Password")}
         
-        res.clearCookie('auth_token')
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true, 
+            domain: "localhost",
+            signed: true,
+            path: "/",
+        })
+
+
         const token = createToken(user._id.toString(), user.email, "7d");
         const expires = new Date();
         expires.setDate(expires.getDate()+7);
-        res.cookie("auth_token", token, {path:"/", domain:"localhost", expires,httpOnly:true, signed:true,})
+        res.cookie(COOKIE_NAME, token, {path:"/", domain:"localhost", expires,httpOnly:true, signed:true,})
+
+
         return res.status(200).json({message:"OK", id: user._id.toString()})
     }catch(error){
         console.log(error)
