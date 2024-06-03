03de82825583e60081f1a2360545a18b2d77cb2b
ym <you@example.com>
2024-06-04 01:37:11 +0530

    Some navigation link, material ui component in use

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,46 @@
+import AppBar from "@mui/material/AppBar"
+import { Toolbar } from '@mui/material';
+import Logo from './shared/Logo';
+import { useAuth } from '../context/AuthContext';
+import NavigationLink from './shared/NavigationLink';
+
+const Header = () => {
+  const auth = useAuth();
+  return (<AppBar sx={{bgcolor:"transparent", position: "static", boxShadow:"none"}}>
+    <Toolbar sx={{display:"flex" }}>
+      <Logo />
+      <div>{auth?.isLoggedIn ? 
+      (<>
+        <NavigationLink bg='#5905e2'
+        to='/chat'
+        text='Go To Chat'
+        textColor='white'          
+        />
+        <NavigationLink 
+          bg='#51538f'
+          to='/'
+          text='Logout'
+          textColor='white' 
+          onClick={auth.logout}
+        />
+      </> )
+      : 
+      (<>
+        <NavigationLink bg='#5905e2'
+        to='/login'
+        text='Login'
+        textColor='white'          
+        />
+        <NavigationLink 
+          bg='#51538f'
+          to='/signup'
+          text='Signup'
+          textColor='white' 
+        />
+      </>)}</div>
+    </Toolbar>
+  </AppBar>
+  )
+}
+
+export default Header
